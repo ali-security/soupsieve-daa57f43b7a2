5acaa4f6d683b888770ebcf3ff614e7c67a4abd1
Seal <info@sealsecurity.io>
2026-09-15 07:53:31 +0000

    CVE-2026-49477

--- tests/test_extra/test_attribute.py
+++ tests/test_extra/test_attribute.py
@@ -1,5 +1,7 @@
 """Test attribute selectors."""
 from .. import util
+import threading
+import soupsieve as sv
 
 
 class TestAttribute(util.TestCase):
@@ -50,3 +52,26 @@ class TestAttribute(util.TestCase):
             ["div", "0", "1", "2", "3", "pre", "4", "6"],
             flags=util.HTML5
         )
+
+    def test_bad_attribute_unclosed(self):
+        """Test bad attribute fails for syntax error, not a catastrophic backtracking timeout."""
+
+        results = []
+
+        def compile_unclosed():
+            """Compile attribute selectors whose quoted value is never closed."""
+
+            for quote in ('"', "'"):
+                try:
+                    sv.compile('[a=' + quote + ('x' * 300))
+                except sv.SelectorSyntaxError:
+                    results.append(True)
+                except Exception:
+                    results.append(False)
+
+        thread = threading.Thread(target=compile_unclosed)
+        thread.daemon = True
+        thread.start()
+        thread.join(30)
+        self.assertFalse(thread.is_alive())
+        self.assertEqual(results, [True, True])
